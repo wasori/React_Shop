9bb19565191d26a8f011d4b930058f9015dd23f0
wasori <wasori@naver.com>
2024-09-27 17:59:52 +0900

    router

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import { Button, Navbar, Container, Nav, Col, Row  } from 'react-bootstrap';
 import bg from './img/bg.png'
 import data from './data.js'
+import Detail from './routes/Detail.js'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 
 function App() {
 
   let [shoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -14,24 +17,38 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg' style={{ backgroundImage : 'url('+ bg +')' }}></div>
-      <Container>
-      <Row>
-        {
-          shoes.map(function(a,i){
-            return(
-              <Product shoes={shoes} i={i}/>
-            )
-          })
-        }
-      </Row>
-    </Container>
+
+
+      <Routes>
+        <Route path='/' element={
+          <>
+          <div className='main-bg' style={{ backgroundImage : 'url('+ bg +')' }}></div>
+          <Container>
+          <Row>
+            {
+              shoes.map(function(a,i){
+                return(
+                  <Product shoes={shoes} i={i}/>
+                )
+              })
+            }
+          </Row>
+          </Container>
+          </>
+        }/>
+        <Route path="/detail" element={<Detail/>}/>
+
+        {/* <Route path="/event" element={<Event/>}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}/>
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>}/>
+        </Route>
+        <Route path="*" element={<div>없는페이지요</div>}/> */}
+      </Routes>
     </div>
   );
 }
@@ -43,6 +60,15 @@ function Product(props){
       <h4>{ props.shoes[props.i].title }</h4>
       <p>{ props.shoes[props.i].content }</p>
     </Col>
+  )
+}
+
+function Event(){
+  return(
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
   )
 }
 
